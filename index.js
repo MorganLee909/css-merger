03c61a2782793e3369087a78ef427f0137e72456
Lee Morgan <me@leemorgan.io>
2021-04-21 12:48:49 +0300

    Add the ability to specify folders. Add recursive searching through folders. Will now only read css files.

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 const fs = require("fs");
 
-module.exports = (files, output)=>{
+let readDirectoryFiles = (dir)=>{
+    let files = fs.readdirSync(dir);
+    files = files.filter(item => !(/(^|\/)\.[^\/\.]/g).test(item));
+
     let css = "";
     for(let i = 0; i < files.length; i++){
-        css += fs.readFileSync(files[i]);
+        let file = `${dir}${files[i]}`;
+        if(file.substring(file.length - 4, file.length) === ".css"){
+            css += fs.readFileSync(`${file}`);
+        }else if(fs.lstatSync(file).isDirectory() === true){
+            css += readDirectoryFiles(`${file}/`);
+        }
+    }
+
+    return css;
+}
+
+let merge = (files, output)=>{
+    let css = "";
+    for(let i = 0; i < files.length; i++){
+        if(files[i].substring(files[i].length - 4, files[i].length) === ".css"){
+            css += fs.readFileSync(files[i]);
+        }else if(fs.lstatSync(files[i]).isDirectory() === true){
+            if(files[i][files[i].length-1] !== "/") files[i] += "/";
+            css += readDirectoryFiles(files[i]);
+        }
     }
 
     // css = css.replace(/\s+/g, "");
 
     fs.writeFileSync(output, css);
 }
+
+module.exports = merge;
